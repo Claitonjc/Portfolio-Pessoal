0c60db86ada312398982842358bfda905f8e7add
Claitonjc <claitoncharlene@outlook.com>
2025-02-11 19:14:22 -0300

    envio do projeto

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
-const button = document.querySelector(".button__slider");
-const personarInfo = document.querySelector(".personal__info-container");
-const professionalInfo = document.querySelector(".professional__info-container");
+const menuList = document.querySelector(".menu__bar-list");
+const sections = Array.from(document.querySelectorAll(".section__container"));
+let currentSection = document.querySelector(".menu__active");
 
+const showSection = (id) => {
+  const nextSection = document.getElementById(id);
 
-const buttonSlide = () => {
-  if (button) {
-    button.addEventListener("click", () => {
-      button.classList.toggle("active");
-      toggleButtonText();
-      showDisplayInfo();
-    });
-  }
+  if (!nextSection || currentSection === nextSection) return;
+
+  const currentIndex = sections.indexOf(currentSection);
+  const nextIndex = sections.indexOf(nextSection);
+  const direction = nextIndex > currentIndex ? "right" : "left";
+
+  const exitAnimation =
+    direction === "right"
+      ? "section__slide-out_right"
+      : "section__slide-out_left";
+  const enterAnimation =
+    direction === "right" ? "section__slide-in_left" : "section__slide-in_right";
+
+    if(currentSection) {
+      currentSection.classList.add(exitAnimation);
+      currentSection.addEventListener(
+        "animationend",
+        () => {
+          currentSection.classList.remove("menu__active", exitAnimation);
+          nextSection.classList.add("menu__active", enterAnimation);
+
+          nextSection.addEventListener("animationend", () => {
+            nextSection.classList.remove(enterAnimation);
+          }, { once: true });
+          currentSection = nextSection;
+        },
+        { once: true }
+      );
+    } else {
+      nextSection.classList.add('menu__active', enterAnimation);
+      currentSection = nextSection;
+
+      nextSection.addEventListener("animationend", () => {
+        nextSection.classList.remove(enterAnimation);
+      }, { once: true });
+    }
 }
-buttonSlide();
 
-const toggleButtonText = () => {
-  
-  if (button.classList.contains("active")) {
-    button.innerHTML = "Profissional";
-  } else {
-    button.innerHTML = "Pessoal";
-  }
-}
-
-const showDisplayInfo = () => {
-  if (button.classList.contains("active")) {
-    personarInfo.style.display = "none";
-    professionalInfo.style.display = "flex";
-  } else {
-    personarInfo.style.display = "flex";
-    professionalInfo.style.display = "none";
-  }
-}
+const sectionId = () => {
+  menuList.addEventListener("click", (event) => {
+    const selectedId = event.target.dataset.section;
+    if (selectedId) {
+      showSection(selectedId);
+    }
+  });
+};
+sectionId();
+showSection("about__me");
